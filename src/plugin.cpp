608a90c19396520c0b8b0b7b61910c9279b12535
artemyv <artemyv@gmail.com>
2025-06-10 20:59:08 +0300

    add logs

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,11 +1,22 @@
+#include "logger.h"
+
+using logi = SKSE::log::info;
+
 SKSEPluginLoad(const SKSE::LoadInterface *skse) {
     SKSE::Init(skse);
+    // Setup logging (e.g. using spdlog)
+    SetupLog();
+
+    SKSE::log::info("Plugin loaded");
 
     // This example prints "Hello, world!" to the Skyrim ~ console.
     // To view it, open the ~ console from the Skyrim Main Menu.
     SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message *message) {
         if (message->type == SKSE::MessagingInterface::kDataLoaded)
+        {
             RE::ConsoleLog::GetSingleton()->Print("Hello, world!");
+            logi("kDataLoaded message received, Hello, world! message sent to console");
+        }
     });
 
     return true;
